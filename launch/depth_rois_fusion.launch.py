1f117eee67a2d7ba18b12f1c79ec3e559a8c662b
minamidani <149769816+minamidani@users.noreply.github.com>
2025-06-24 18:48:55 +0900

    launchする際にconfig.yamlから設定を読むように変更

--- launch/depth_rois_fusion.launch.py
+++ launch/depth_rois_fusion.launch.py
@@ -1,25 +1,30 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import yaml
+import os
+
+def load_param_yaml_section(path, section):
+    with open(path, 'r') as f:
+        all_params = yaml.safe_load(f)
+    return all_params.get(section, {})
 
 def generate_launch_description():
+    config_path = os.path.join(
+        get_package_share_directory('depth_rois_fusion'),
+        'config',
+        'config.yaml'
+    )
+
+    params = load_param_yaml_section(config_path, 'depth_rois_fusion_node')
+
     return LaunchDescription([
         Node(
             package='depth_rois_fusion',
             executable='depth_rois_fusion_node',
             name='depth_rois_fusion_node',
             output='screen',
-            parameters=[{
-                "pointcloud_topic": "/sensing/lidar/concatenated/pointcloud",
-                "camera_info_topic": "/sensing/camera/roscube/front_wide/camera_info",
-                "rois_topic": "/perception/object_recognition/detection/rois0",
-                "depth_image_topic": "/camera/depth_image",
-                "depth_rois_image_topic": "/camera/depth_rois_image",
-                "out_camera_info_topic": "/camera/camera_info",
-                "rectangle_color": [0, 255, 0],
-                "text_color": [255, 0, 0],
-                "rois_depth_msg_topic": "/camera/rois_depth",
-                "depth_rois_marker_topic": "/camera/rois_depth_marker"
-            }]
+            parameters=[params]
         )
     ])
 
